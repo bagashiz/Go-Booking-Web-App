511bdca2e5feed68bc660307fa51b69115aadb77
bagashiz <bagashiz@proton.me>
2022-08-07 23:19:54 +0700

    testing POST handlers

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -25,6 +26,20 @@ var theTests = []struct {
 	{"contact", "/contact", "GET", nil, http.StatusOK},
 	{"search-availability", "/search-availability", "GET", nil, http.StatusOK},
 	{"make-reservation", "/make-reservation", "GET", nil, http.StatusOK},
+	{"post-search-availability", "/search-availability", "POST", []postData{
+		{key: "checkInDate", value: "2020-01-01"},
+		{key: "checkOutDate", value: "2020-01-02"},
+	}, http.StatusOK},
+	{"post-search-availability-json", "/search-availability-json", "POST", []postData{
+		{key: "checkInDate", value: "2020-01-01"},
+		{key: "checkOutDate", value: "2020-01-02"},
+	}, http.StatusOK},
+	{"make-reservation-post", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "John"},
+		{key: "last_name", value: "Doe"},
+		{key: "email", value: "john@doe.com"},
+		{key: "phone", value: "111222333"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(t *testing.T) {
@@ -35,6 +50,22 @@ func TestHandlers(t *testing.T) {
 	for _, e := range theTests {
 		if e.method == "GET" {
 			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("For %s, expected %d, got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+
+		} else { // POST
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
 			if err != nil {
 				t.Log(err)
 				t.Fatal(err)
